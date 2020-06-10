af50d282f76fd2c13a1e9c21b4f93f223c966b8e
nikitanesterenkos@gmail.com <piramida741147>
2020-06-10 14:42:16 +0300

    Initial commit

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,10 +1,12 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
-
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
+
+
+
     public UserDaoHibernateImpl() {
 
     }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,26 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+
+    private static final String URL = "jdbc:mysql://localhost:3306/coretasktemplate?serverTimezone=UTC";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "09po:L?>";
+    private static final String DRIVER = "com.mysql.jdbc.Driver";
+
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            Class.forName(DRIVER);
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+
+        } catch (ClassNotFoundException | SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+        return connection;
+    }
 }
